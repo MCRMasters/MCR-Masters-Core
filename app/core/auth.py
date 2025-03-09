7250e2df02c1f23c773eb39f568d16f4286e87c2
Taeho <taehogwon96@gmail.com>
2025-03-10 01:32:22 +0900

    fix get current user

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,14 +7,16 @@ from app.core.security import get_user_id_from_token
 from app.db.session import get_session
 from app.models.user import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/auth/login")
+auth_scheme = HTTPBearer()
 
 
 async def get_current_user(
-    token: str = Depends(oauth2_scheme),
+    auth: HTTPAuthorizationCredentials = Depends(auth_scheme),
     session: AsyncSession = Depends(get_session),
 ) -> User:
+    token = auth.credentials
     user_id = get_user_id_from_token(token)
+
     if not user_id:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
