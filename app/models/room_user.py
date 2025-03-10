5a9adb91abed5bb0833edcf78ae5bb0e36d3492d
Taeho <taehogwon96@gmail.com>
2025-03-10 15:40:30 +0900

    add id for roomuser model

--- app/models/room_user.py
+++ app/models/room_user.py
@@ -1,12 +1,16 @@
-from uuid import UUID
+from uuid import UUID, uuid4
 
 from sqlalchemy import UniqueConstraint
 from sqlmodel import Field, SQLModel
 
 
 class RoomUser(SQLModel, table=True):  # type: ignore[call-arg]
-    room_id: UUID = Field(foreign_key="room.id", primary_key=True)
-    user_id: UUID = Field(foreign_key="user.id", primary_key=True)
+    id: UUID = Field(
+        default_factory=uuid4,
+        primary_key=True,
+    )
+    room_id: UUID = Field(foreign_key="room.id")
+    user_id: UUID = Field(foreign_key="user.id")
     is_ready: bool = Field(default=False)
 
     __table_args__ = (UniqueConstraint("user_id"),)
