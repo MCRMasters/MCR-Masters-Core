7fb15c40c9c14ebefb2e6442aa1c981db1811b77
Taeho <taehogwon96@gmail.com>
2025-03-12 16:06:21 +0900

    refactor google api

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db.session import get_session
+from app.dependencies.services import get_google_oauth_service
 from app.schemas.auth_url_response import AuthUrlResponse
 from app.schemas.token_response import TokenResponse
 from app.services.auth.google import GoogleOAuthService
@@ -11,9 +10,8 @@ router = APIRouter()
 
 @router.get("/login/google", response_model=AuthUrlResponse)
 async def google_login(
-    session: AsyncSession = Depends(get_session),
+    google_service: GoogleOAuthService = Depends(get_google_oauth_service),
 ):
-    google_service = GoogleOAuthService(session)
     auth_url = google_service.get_authorization_url()
     return AuthUrlResponse(auth_url=auth_url)
 
@@ -21,7 +19,6 @@ async def google_login(
 @router.get("/login/google/callback", response_model=TokenResponse)
 async def google_callback(
     code: str,
-    session: AsyncSession = Depends(get_session),
+    google_service: GoogleOAuthService = Depends(get_google_oauth_service),
 ):
-    google_service = GoogleOAuthService(session)
     return await google_service.process_google_login(code)
